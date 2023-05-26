d446bf70f03a958ff3dbce3a1f94da1f20a3330f
donabe <donabe722@gmail.com>
2023-05-26 19:55:25 +0900

    #2:ダメージ処理追加

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerStatus : MonoBehaviour
+    {
+        [SerializeField] private float maxHP;
+
+        [Header("now status")]
+        [SerializeField] private float nowHP;
+
+        [SerializeField] private PlayerHitCheck _hitCheck;
+
+        private void Start()
+        {
+            nowHP = maxHP;
+
+            _hitCheck.DamageAction += AddDamageAction;
+        }
+
+        private void AddDamageAction(float damage)
+        {
+            if (nowHP <= 0) return;
+            
+            nowHP -= damage;
+
+            if (nowHP <= 0)
+            {
+                Dead();
+            }
+        }
+
+        private void Dead()
+        {
+            Debug.Log("死んだ");
+        }
+    }
+}
