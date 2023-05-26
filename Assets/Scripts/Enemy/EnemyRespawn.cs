5de5c3d341428e6da724f6aa5954c80e1e9d1880
matumoto1234 <matumotosizuoka@gmail.com>
2023-05-27 00:58:26 +0900

    #12:Enemyディレクトリにスクリプトをまとめた

--- Assets/Scripts/Enemy/EnemyRespawn.cs
+++ Assets/Scripts/Enemy/EnemyRespawn.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Respawn : MonoBehaviour
+public class EnemyRespawn : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
     
