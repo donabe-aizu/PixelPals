a66dead62d116c431117e3ec1a946c692d7ee621
matumoto1234 <matumotosizuoka@gmail.com>
2023-05-31 17:38:07 +0900

    #12:format document

--- Assets/Scripts/Enemy/EnemyRespawn.cs
+++ Assets/Scripts/Enemy/EnemyRespawn.cs
@@ -1,47 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyRespawn : MonoBehaviour
 {
-    [SerializeField] GameObject enemy;
-    
-    [SerializeField] float respawnDuration;
-    
-    [SerializeField] int limitRespawnCount;
-    
+    [SerializeField] private GameObject enemy;
+
+    [SerializeField] private float respawnDuration;
+
+    [SerializeField] private int limitRespawnCount;
+
     //　今何人の敵を出現させたか（総数）
     private int respawnedEnemyCount;
-    
+
     //　待ち時間計測フィールド
     private float respawnElapsedTime;
- 
+
     // Use this for initialization
-    void Start () {
+    private void Start()
+    {
         respawnedEnemyCount = 0;
         respawnElapsedTime = 0f;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (respawnedEnemyCount >= limitRespawnCount) {
-            return;
-        }
-        
+        if (respawnedEnemyCount >= limitRespawnCount) return;
+
         respawnElapsedTime += Time.deltaTime;
- 
+
         //　経過時間が経ったら
-        if (respawnElapsedTime > respawnDuration) {
+        if (respawnElapsedTime > respawnDuration)
+        {
             respawnElapsedTime = 0f;
- 
+
             RespawnEnemy();
-        } 
+        }
     }
-    
+
     //　敵出現メソッド
-    void RespawnEnemy() {
-        GameObject.Instantiate(enemy, transform.position, Quaternion.Euler (0f, 0f, 0f));
+    private void RespawnEnemy()
+    {
+        Instantiate(enemy, transform.position, Quaternion.Euler(0f, 0f, 0f));
         respawnedEnemyCount++;
         respawnElapsedTime = 0f;
     }
