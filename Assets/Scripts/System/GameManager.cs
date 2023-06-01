8a658922925a4cd0405417a7e2f9d8737efe2ae7
donabe <donabe722@gmail.com>
2023-06-02 04:00:19 +0900

    #50-三人称視点カメラ追加、カーソル消す処理

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -8,16 +8,40 @@ namespace System
     {
         [SerializeField] private PlayerStatus _playerStatus;
         [SerializeField] private EnemyBaseStatus _enemyBaseStatus;
+        [SerializeField] private GameObject _gameMenu;
         [SerializeField] private GameObject _gameClearUI;
         [SerializeField] private GameObject _gameOverUI;
 
         private Pose _initialPlayerPose;
 
+        private bool _isOpenMenu = false;
+
         private void Start()
         {
-            _initialPlayerPose = new Pose(_playerStatus.transform.position,_playerStatus.transform.rotation);
+            _initialPlayerPose = new Pose(_playerStatus.transform.position, _playerStatus.transform.rotation);
             _playerStatus.PlayerHPChangeAction += PlayerHP;
             _enemyBaseStatus.ChangeHP += EnemyBaseHP;
+            
+            ChangeCursorVisible(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (_isOpenMenu)
+                {
+                    _isOpenMenu = false;
+                    _gameMenu.SetActive(false);
+                    ChangeCursorVisible(false);
+                }
+                else
+                {
+                    _isOpenMenu = true;
+                    _gameMenu.SetActive(true);
+                    ChangeCursorVisible(true);
+                }
+            }
         }
 
         private void PlayerHP(float nowHP)
@@ -34,6 +58,12 @@ namespace System
             {
                 GameClear();
             }
+        }
+
+        private void ChangeCursorVisible(bool isVisible)
+        {
+            Cursor.visible = isVisible;
+            Cursor.lockState = isVisible ? CursorLockMode.Locked : CursorLockMode.Confined;
         }
 
         private void GameClear()
